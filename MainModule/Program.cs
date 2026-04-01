07776b9152e9510d4cb5307fc428eab493285463
Evaristo Cuesta <SESA639503@se.com>
2026-04-01 13:11:31 +0200

    Add core library

--- MainModule/Program.cs
+++ MainModule/Program.cs
@@ -1,8 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//using LibraryModule;
+using LibraryModule;
 
 Console.WriteLine("Hello, World!");
-//Console.WriteLine($"Add {CoreLibrary.Add(5, 10)}");
-//Console.WriteLine($"Subtract {CoreLibrary.Subtract(10, 5)}");
-//Console.WriteLine($"Multiply {CoreLibrary.Multiply(5, 10)}");
+Console.WriteLine($"Add {CoreLibrary.Add(5, 10)}");
+Console.WriteLine($"Subtract {CoreLibrary.Subtract(10, 5)}");
+Console.WriteLine($"Multiply {CoreLibrary.Multiply(5, 10)}");
 //Console.WriteLine($"Divide {CoreLibrary.Divide(10, 5)}");

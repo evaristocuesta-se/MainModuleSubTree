b06e9e930bf16c28fde6ad5ec52c3e18959f0bfa
Evaristo Cuesta <SESA639503@se.com>
2026-04-01 12:48:02 +0200

    First commit

---
+++ MainModule/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+//using LibraryModule;
+
+Console.WriteLine("Hello, World!");
+//Console.WriteLine($"Add {CoreLibrary.Add(5, 10)}");
+//Console.WriteLine($"Subtract {CoreLibrary.Subtract(10, 5)}");
+//Console.WriteLine($"Multiply {CoreLibrary.Multiply(5, 10)}");
+//Console.WriteLine($"Divide {CoreLibrary.Divide(10, 5)}");

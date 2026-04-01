13e9748422490538f28e52414b6924dea5e5f37a
Evaristo Cuesta <SESA639503@se.com>
2026-04-01 13:04:51 +0200

    Squashed 'libs/LibraryModule/' content from commit 678a4f7
    git-subtree-dir: libs/LibraryModule
    git-subtree-split: 678a4f771f8a30e1a4764fc03ad715cf8076b9fc

---
+++ CoreLibrary.cs
@@ -0,0 +1,19 @@
+﻿namespace LibraryModule;
+
+public class CoreLibrary
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
